c190f7c343eaff51a6dec76914a6be6a7c5638cc
Sam O'Leary <ssjoleary@gmail.com>
2014-02-17 22:43:44 +0000

    Update model: add extra fields

--- speciesguide/models.py
+++ speciesguide/models.py
@@ -4,8 +4,13 @@ from django.db import models
 
 class Species(models.Model):
 	specname = models.CharField(max_length=200)
-	specclass = models.CharField(max_length=200, default="Mammalia")
-	specorder = models.CharField(max_length=200, default="Cetecea")
+	specclass = models.CharField(max_length=200)
+	specorder = models.CharField(max_length=200)
+	specsuborder = models.CharField(max_length=200)
+	specfamily = models.CharField(max_length=200)
+	specgenus = models.CharField(max_length=200)
+	specspecies = models.CharField(max_length=200)
+	speccommonname = models.CharField(max_length=200)
 
 	def __unicode__(self):
 		return self.specname 
