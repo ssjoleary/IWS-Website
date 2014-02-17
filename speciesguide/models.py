374263ebd320ee52758a13d85bacb1a58c1b18cd
Sam O'Leary <ssjoleary@gmail.com>
2014-02-17 23:23:39 +0000

    Update models: add default values

--- speciesguide/models.py
+++ speciesguide/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Species(models.Model):
-	specname = models.CharField(max_length=200)
-	specclass = models.CharField(max_length=200)
-	specorder = models.CharField(max_length=200)
-	specsuborder = models.CharField(max_length=200)
-	specfamily = models.CharField(max_length=200)
-	specgenus = models.CharField(max_length=200)
-	specspecies = models.CharField(max_length=200)
-	speccommonname = models.CharField(max_length=200)
+	specname = models.CharField(max_length=200, default="editspecname")
+	specclass = models.CharField(max_length=200, default="editspecclass")
+	specorder = models.CharField(max_length=200, default="editspecorder")
+	specsuborder = models.CharField(max_length=200, default="editspecsuborder")
+	specfamily = models.CharField(max_length=200, default="editspecfamily")
+	specgenus = models.CharField(max_length=200, default="editspecgenus")
+	specspecies = models.CharField(max_length=200, default="editspecspecies")
+	speccommonname = models.CharField(max_length=200, default="editspeccommonname")
 
 
 	def __unicode__(self):
