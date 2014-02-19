214c2323384509b3887579ed3525d193ed4cac41
Sam O'Leary <ssjoleary@gmail.com>
2014-02-19 18:07:04 +0000

    JSON Text 1.0

--- sightings/urls.py
+++ sightings/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /species/5
     url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+
+    url(r'^getsighting/$', views.get_sighting, name='get_sighting'),
     )
--- sightings/views.py
+++ sightings/views.py
@@ -4,6 +4,9 @@ from django.core.urlresolvers import reverse
 
 from sightings.models import Sighting
 
+from django.utils import simplejson
+from django.http import HttpResponse
+
 # Create your views here.
 
 class IndexView(generic.ListView):
@@ -17,3 +20,11 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Sighting
     template_name = 'sightings/detail.html'
+
+def get_sighting(request):
+    result = []
+
+    result.append({"User":"Sam O'Leary"})
+    result.append({"Occupation":"Student"})
+
+    return HttpResponse(simplejson.dumps(result), mimetype='text/plain')
