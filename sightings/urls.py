29118de3e2e193ce87583ce8511a3ecc47e666ce
Sam O'Leary <ssjoleary@gmail.com>
2014-02-19 15:05:37 +0000

    Create Url patterns for Index & Detail views

---
+++ sightings/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from sightings import views
+
+urlpatterns = patterns('',
+    # ex: /species/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # ex: /species/5
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    )
