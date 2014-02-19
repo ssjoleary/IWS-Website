ccee85ad9f38cff9f218fe35b4aa4a39284a32a6
Sam O'Leary <ssjoleary@gmail.com>
2014-02-19 15:15:03 +0000

    Add Selenium 2.39.0

--- speciesguide/urls.py
+++ speciesguide/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from speciesguide import views
 
 urlpatterns = patterns('',
-	# ex: /species/
-	url(r'^$', views.IndexView.as_view(), name='index'),
-	# ex: /species/5
-	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
-)
+    # ex: /species/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # ex: /species/5
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    )
