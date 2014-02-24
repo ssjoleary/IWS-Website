318e08a0fcd767a039019eed5af24a14cf7ffd98
Sam O'Leary <ssjoleary@gmail.com>
2014-02-24 23:51:43 +0000

    Remove unused imports

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.views import generic
-from django.core.urlresolvers import reverse
 
 # Create your views here.
+
 
 def index(request):
     template_name = 'homepage/index.html'
