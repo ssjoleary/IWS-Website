bbc0437e17be9483187a996d42af22f4a52296c6
Sam O'Leary <ssjoleary@gmail.com>
2014-02-24 20:50:57 +0000

    Correct indentation

--- sightings/models.py
+++ sightings/models.py
@@ -3,13 +3,14 @@ from speciesguide.models import Species
 
 # Create your models here.	
 
-class Sighting(models.Model):
-	sub_date = models.CharField(max_length=200, default='date submitted')
-	species = models.ForeignKey('speciesguide.Species')
-	animals = models.IntegerField(default=0)
-	location = models.CharField(max_length=200, default='editLocation')
-	latitude = models.DecimalField(max_digits=5, decimal_places=3, default=0)
-	longitude = models.DecimalField(max_digits=5, decimal_places=3, default=0)
 
-	def __unicode__(self):
-		return self.location	
+class Sighting(models.Model):
+    sub_date = models.CharField(max_length=200, default='date submitted')
+    species = models.ForeignKey('speciesguide.Species')
+    animals = models.IntegerField(default=0)
+    location = models.CharField(max_length=200, default='editLocation')
+    latitude = models.DecimalField(max_digits=5, decimal_places=3, default=0)
+    longitude = models.DecimalField(max_digits=5, decimal_places=3, default=0)
+
+    def __unicode__(self):
+        return self.location
