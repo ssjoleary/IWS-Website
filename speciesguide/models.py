01b9152ad2d448bf81451c8e709ede13624941d2
Sam O'Leary <ssjoleary@gmail.com>
2014-02-27 18:10:27 +0000

    Create a class & method to handle the use of natural keys

--- speciesguide/models.py
+++ speciesguide/models.py
@@ -2,16 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+
+class SpeciesManager(models.Manager):
+    def get_by_natural_key(self, specname):
+        return self.get(specname=specname)
+
+
 class Species(models.Model):
-	specname = models.CharField(max_length=200, default="editspecname")
-	specclass = models.CharField(max_length=200, default="editspecclass")
-	specorder = models.CharField(max_length=200, default="editspecorder")
-	specsuborder = models.CharField(max_length=200, default="editspecsuborder")
-	specfamily = models.CharField(max_length=200, default="editspecfamily")
-	specgenus = models.CharField(max_length=200, default="editspecgenus")
-	specspecies = models.CharField(max_length=200, default="editspecspecies")
-	speccommonname = models.CharField(max_length=200, default="editspeccommonname")
+    specname = models.CharField(max_length=200, default="editspecname", unique=True)
+    specclass = models.CharField(max_length=200, default="editspecclass")
+    specorder = models.CharField(max_length=200, default="editspecorder")
+    specsuborder = models.CharField(max_length=200, default="editspecsuborder")
+    specfamily = models.CharField(max_length=200, default="editspecfamily")
+    specgenus = models.CharField(max_length=200, default="editspecgenus")
+    specspecies = models.CharField(max_length=200, default="editspecspecies")
+    speccommonname = models.CharField(max_length=200, default="editspeccommonname")
 
+    def __unicode__(self):
+        return self.specname
 
-	def __unicode__(self):
-		return self.specname 
+    def natural_key(self):
+        return self.specname
--- speciesguide/views.py
+++ speciesguide/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render
 from django.views import generic
-from django.core.urlresolvers import reverse
 
 from speciesguide.models import Species
 
 # Create your views here.
 
-class IndexView(generic.ListView):
-	template_name = 'speciesguide/index.html'
-	context_object_name = 'species_list'
 
-	def get_queryset(self):
-		# Return the last five.
-		return Species.objects.order_by('specname')[:25]
+class IndexView(generic.ListView):
+    template_name = 'speciesguide/index.html'
+    context_object_name = 'species_list'
+
+    def get_queryset(self):
+        # Return the last twenty five.
+        return Species.objects.order_by('specname')[:25]
+
 
 class DetailView(generic.DetailView):
-	model = Species
-	template_name = 'speciesguide/detail.html'
+    model = Species
+    template_name = 'speciesguide/detail.html'
