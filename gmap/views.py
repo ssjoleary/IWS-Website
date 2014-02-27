841802ccb16b26a8a56fd2b9c4489b9737c911d8
Sam O'Leary <ssjoleary@gmail.com>
2014-02-27 18:09:15 +0000

    Edit formatting & whitespace & sort imports

--- gmap/views.py
+++ gmap/views.py
@@ -1,19 +1,17 @@
-from django.shortcuts import render
 from django.views import generic
-from django.core.urlresolvers import reverse
+from django.core import serializers
+from django.shortcuts import render_to_response
 
 from sightings.models import Sighting
 
 # Create your views here.
+
 
 class IndexView(generic.ListView):
     template_name = 'gmap/index.html'
     context_object_name = 'sightings_list'
 
     def get_queryset(self):
-        # Return the last five.
+    # Return the last twenty five.
         return Sighting.objects.order_by('sub_date')[:25]
 
-#def index(request):
-#    template_name = 'gmap/index.html'
-#    return render(request, template_name)
