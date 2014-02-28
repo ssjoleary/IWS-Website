99151444ea4d7380678f18357b330fac116914ac
Sam O'Leary <ssjoleary@gmail.com>
2014-02-28 17:21:18 +0000

    Reorganise code & include handlers to retrieve search value from the search dropdown and fetch search results via JSON

--- wildlifewebapp/static/js/gmap.js
+++ wildlifewebapp/static/js/gmap.js
@@ -2,69 +2,187 @@
  * Created by ssjoleary on 26/02/14.
  */
 
-$(function(){
+$(function($,W,D,undefined)
+{
     var mapOptions
     var map
     var centerLatLng
+    var markers = [];
 
-    mapOptions = {
-        center: new google.maps.LatLng(53.41608, -7.93396),
-        zoom: 6
-    };
-    map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
-    //attach a jQuery live event to the button
+    GMAP = {
+        name: "Gmap Sightings",
+        namespace: "GMAP",
+        settings: {
+
+        },
+        cache: {
+
+        },
+
+        init: function(){
+            mapOptions = {
+                center: new google.maps.LatLng(53.41608, -7.93396),
+                zoom: 6
+            };
+            map = new google.maps.Map(document.getElementById("map-canvas"),
+                mapOptions);
+            //attach a jQuery live event to the button
+
+
+            $.getJSON("/sightings/getsighting", function( data ) {
+                centerLatLng = new google.maps.LatLng(53.41608,-7.93396)
+
+                $.each(data, function(index, element){
+                    var sightingLatLng = new google.maps.LatLng(element.fields.latitude, element.fields.longitude)
+                    // To add the marker to the map, use the 'map' property
+                    var marker = new google.maps.Marker({
+                        position: sightingLatLng,
+                        map: map,
+                        title: element.fields.location,
+                        animation: google.maps.Animation.DROP
+                    });
+                    var contentString = '<h1><small>'+element.fields.sub_date+'</small></h1>' +
+                        '<h2><small>Species:</small></h2><p>'+element.fields.species+'</p>'+
+                        '<h2><small>Animals:</small></h2><p>'+element.fields.animals+'</p>'+
+                        '<h2><small>Location:</small></h2><p>'+element.fields.location+'</p>'+
+                        '<h2><small>Latitude:</small></h2><p>'+element.fields.latitude+'</p>'+
+                        '<h2><small>Longitude:</small></h2><p>'+element.fields.longitude+'</p>';
+
+                    var infowindow = new google.maps.InfoWindow({
+                        content: contentString
+                    });
+                    google.maps.event.addListener(marker, 'click', function() {
+
+                        infowindow.open(map,marker);
+                    });
+                    google.maps.event.addListener(infowindow, 'closeclick', function() {
+                        infowindow.close(map,marker)
+                        map.panTo(centerLatLng)
+                        map.setZoom(6)
+                    });
+                    markers.push(marker)
+                });
+                $.each(data, function(index, element) {
+                    var $tableData = $('<tr>').append(
+                        $('<td>').text(element.fields.sub_date),
+                        $('<td>').text(element.fields.species),
+                        $('<td>').text(element.fields.animals),
+                        $('<td>').text(element.fields.location),
+                        $('<td>').text(+element.fields.latitude),
+                        $('<td>').text(element.fields.longitude)
+                    ).appendTo('#sightingsTablebody')
+                });
+
+            });
+            this.setupEventHandlers();
+        },
+
+        setupEventHandlers: function(){
+            var _this = this;
+
+            $('#addSightingbtn').on('click', function()
+            {
+                $('#myModal').modal('show')
+            });
+
+            $(".dropdown-menu li a").on('click', function()
+            {
+                var selText = $(this).text();
+                $(this).parents('.dropdown').find('.dropdown-toggle').html(selText+'<span class="caret"></span>');
+            });
+
+
+            $('#pictureInput').on('change', function(e)
+            {
+                e.preventDefault();
+                console.log('getting image...');
+                _this.uploadImage();
+            });
+
+            $('#dropdownSearch li a').on('click', function()
+            {
+                var search = { county: $(this).text() };
+                $.ajax({
+                    url: '/sightings/getspecificsighting/',
+                    type: 'POST',
+                    contentType: 'application/json; charset=utf-8',
+                    data: JSON.stringify(search),
+                    dataType: 'json',
+                    success: function(data)
+                    {
+                        clearMarkers();
+                        $('#sightingsTablebody').empty();
+                        $.each(data, function(index, element){
+                            var sightingLatLng = new google.maps.LatLng(element.fields.latitude, element.fields.longitude)
+                            // To add the marker to the map, use the 'map' property
+                            var marker = new google.maps.Marker({
+                                position: sightingLatLng,
+                                map: map,
+
+                                title: element.fields.location,
+                                animation: google.maps.Animation.DROP
+                            });
+                            var contentString = '<h1><small>'+element.fields.sub_date+'</small></h1>' +
+                                '<h2><small>Species:</small></h2><p>'+element.fields.species+'</p>'+
+                                '<h2><small>Animals:</small></h2><p>'+element.fields.animals+'</p>'+
+                                '<h2><small>Location:</small></h2><p>'+element.fields.location+'</p>'+
+                                '<h2><small>Latitude:</small></h2><p>'+element.fields.latitude+'</p>'+
+                                '<h2><small>Longitude:</small></h2><p>'+element.fields.longitude+'</p>';
+
+                            var infowindow = new google.maps.InfoWindow({
+                                content: contentString
+                            });
+                            google.maps.event.addListener(marker, 'click', function() {
+
+                                infowindow.open(map,marker);
+                            });
+                            google.maps.event.addListener(infowindow, 'closeclick', function() {
+                                infowindow.close(map,marker)
+                                map.panTo(centerLatLng)
+                                map.setZoom(6)
+                            });
+                            markers.push(marker)
+                        });
+                        $.each(data, function(index, element) {
+                            var $tableData = $('<tr>').append(
+                                $('<td>').text(element.fields.sub_date),
+                                $('<td>').text(element.fields.species),
+                                $('<td>').text(element.fields.animals),
+                                $('<td>').text(element.fields.location),
+                                $('<td>').text(+element.fields.latitude),
+                                $('<td>').text(element.fields.longitude)
+                            ).appendTo('#sightingsTablebody')
+                        });
+                    }
+                });
+            })
+
+            function setAllMap(map) {
+                for (var i = 0; i < markers.length; i++) {
+                    markers[i].setMap(map);
+                }
+            }
+
+            function clearMarkers() {
+                setAllMap(null);
+            }
+        },
+
+        uploadImage: function()
+        {
+            var _this = this,
+                $imgInput = $('#pictureInput');
+
+            $.ajaxFileUpload(
+                {
+
+                }
+            )
+        }
+    }
+
     $(document).ready( function(){
-
-        $.getJSON("/sightings/getsighting", function( data ) {
-
-            centerLatLng = new google.maps.LatLng(53.41608,-7.93396)
-
-            $.each(data, function(index, element){
-                var sightingLatLng = new google.maps.LatLng(element.fields.latitude, element.fields.longitude)
-                // To add the marker to the map, use the 'map' property
-                var marker = new google.maps.Marker({
-                    position: sightingLatLng,
-                    map: map,
-                    title: element.fields.location,
-                    animation: google.maps.Animation.DROP
-                });
-                var contentString = '<h1><small>'+element.fields.sub_date+'</small></h1>' +
-                    '<h2><small>Species:</small></h2><p>'+element.fields.species+'</p>'+
-                    '<h2><small>Animals:</small></h2><p>'+element.fields.animals+'</p>'+
-                    '<h2><small>Location:</small></h2><p>'+element.fields.location+'</p>'+
-                    '<h2><small>Latitude:</small></h2><p>'+element.fields.latitude+'</p>'+
-                    '<h2><small>Longitude:</small></h2><p>'+element.fields.longitude+'</p>';
-
-                var infowindow = new google.maps.InfoWindow({
-                    content: contentString
-                });
-                google.maps.event.addListener(marker, 'click', function() {
-
-                    infowindow.open(map,marker);
-                });
-                google.maps.event.addListener(infowindow, 'closeclick', function() {
-                    infowindow.close(map,marker)
-                    map.panTo(centerLatLng)
-                    map.setZoom(6)
-                })
-
-            });
-            $.each(data, function(index, element) {
-                var $tableData = $('<tr>').append(
-                    $('<td>').text(element.fields.sub_date),
-                    $('<td>').text(element.fields.species),
-                    $('<td>').text(element.fields.animals),
-                    $('<td>').text(element.fields.location),
-                    $('<td>').text(+element.fields.latitude),
-                    $('<td>').text(element.fields.longitude)
-                ).appendTo('#sightingsTablebody')
-            });
-
-        });
+        GMAP.init();
     });
 
-    $('#addSightingbtn').on('click', function() {
-        $('#myModal').modal('show')
-    });
 });
